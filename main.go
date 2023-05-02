9c551b0e79f0dcdb0170603bd10a889fb27f81cc
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-05-01 21:16:03 -0400

    wip

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"image/color"
 	"log"
 	"math"
+	"strings"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -19,7 +21,7 @@ var (
 
 	charX  = 0.0
 	charY  = screenHeight / 2
-	speedX = 2
+	speedX = 3
 )
 
 type Game struct {
@@ -35,24 +37,81 @@ func (g *Game) Update() error {
 		charX = 0
 	}
 
+	if ebiten.IsKeyPressed(ebiten.KeySpace) {
+		showTextBox = true
+	} else {
+		showTextBox = false
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(charX, float64(charY))
-	screen.DrawImage(character, op)
 
-	// backgroundWidth, backgroundHeight := background.Bounds().Size()
-	imgPoint := background.Bounds().Size()
-	backgroundScaleY := float64(screenHeight) / float64(imgPoint.Y)
-
-	for i := -1; i < int(math.Ceil(float64(screenWidth)/float64(imgPoint.X))); i++ {
-		op = &ebiten.DrawImageOptions{}
+	bgImgPoint := background.Bounds().Size()
+	backgroundScaleY := float64(screenHeight) / float64(bgImgPoint.Y)
+	var xMovement float64
+	for i := -1; i < int(math.Ceil(float64(screenWidth)/float64(bgImgPoint.X))); i++ {
+		op := &ebiten.DrawImageOptions{}
 		op.GeoM.Scale(1, backgroundScaleY)
-		op.GeoM.Translate(float64(i*imgPoint.X)-charX, 0)
+
+		xMovement = float64(i*bgImgPoint.X) - charX
+		op.GeoM.Translate(xMovement, 0)
 		screen.DrawImage(background, op)
+
 	}
+
+	drawMalala(screen)
+	printStory(screen, int(xMovement))
+
+}
+
+func printStory(screen *ebiten.Image, screenLocation int) {
+	if screenLocation < -100 && screenLocation > -500 {
+		drawTextBox(screen, "Malala was the youngest Nobel peace price winner!! What would happen if this was a really long string with lots and lots of text, I guess we'd just have to see, how far can we go!? CAN WE GO FARTHER?!? I THINK WE CAN!!!!!! OOOOOOOOOOHHHHWWWWEEEEEEEE RICK, I THINK, OH GEE RICK I THINK WE'RE GOING TO BREAK THIS STRING!!!!!! *BURPRPRPRP* You- you know what your problem is, Morty? You don't know a char pointer when you see one *burp* that's right, there are no strings, Morty! It's arrays! arrays of arrays morty! *burp* They're everywhere! And you can get unsafe morty, you can start doing pointer arithmatic if that's your thing, but you gotta be careful morty, you gotta be careful! You can't just go around dereferencing pointers all willy nilly, you gotta be careful morty!")
+	}
+}
+
+func drawTextBox(screen *ebiten.Image, text string) {
+	// Draw a semi-transparent black rectangle over the entire screen
+	rect := ebiten.NewImage(screenWidth, screenHeight)
+	screen.DrawImage(rect, nil)
+
+	// Draw a border around the text box
+	borderWidth := 2.0
+	borderColor := color.NRGBA{0, 0, 0, 0}
+	rect = ebiten.NewImage(int(screenWidth-(borderWidth*2)), 50)
+	rect.Fill(borderColor)
+	rect.Fill(color.NRGBA{0, 0, 0, 128})
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(borderWidth, float64(screenHeight-50-borderWidth))
+
+	defaultFontPixelWidth := 12
+	maxCharsPerLine := (screenWidth / defaultFontPixelWidth) * 2
+	currentY := 0
+	for i := 0; i < len(text); i += maxCharsPerLine {
+		var stringVal string
+		if i+maxCharsPerLine > len(text) {
+			stringVal = text[i:]
+		} else {
+			stringVal = text[i : i+maxCharsPerLine]
+		}
+
+		ebitenutil.DebugPrintAt(rect, strings.TrimLeft(stringVal, " "), 0, currentY)
+
+		currentY += defaultFontPixelWidth
+	}
+
+	screen.DrawImage(rect, op)
+
+}
+
+func drawMalala(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(0.05, 0.05)
+	op.GeoM.Translate(100, float64(charY))
+	screen.DrawImage(character, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -73,7 +132,7 @@ func main() {
 	}
 
 	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("2D Side Scrolling Game")
+	ebiten.SetWindowTitle("Malala's Story")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
